f4bfa6257fc3e92b11b8e2bde83d8518525db674
Nestor Vera <nestorvera@me.com>
2022-07-05 16:53:42 +0200

    Revert "Remove default page component"
    This reverts commit 29f3022c0c9dda48f5133783bdd09b4975da7934.

---
+++ pages/[...slug].vue
@@ -0,0 +1,8 @@
+<template>
+  <article>
+    <ContentDoc
+      class="grid gap-8"
+      _class="prose max-w-none text-current prose-headings:text-current prose-a:text-current"
+    />
+  </article>
+</template>
