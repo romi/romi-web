1f4a133191590bf4cb44ed1d9f9c806f4feda1fc
Nestor Vera <nestorvera@me.com>
2022-07-05 12:44:26 +0200

    Add logo to `SiteHeader` component

--- components/site/SiteHeader.vue
+++ components/site/SiteHeader.vue
@@ -1,8 +1,10 @@
 <template>
   <header class="z-40 sticky top-0 bg-inherit">
-    <div class="container mx-auto">
+    <div class="flex items-center space-x-8 container mx-auto py-4">
       <div>
-
+        <NuxtLink class="" to="/">
+          <img class="h-12 invert" src="https://media.romi-project.eu/romi-web/assets/images/romi_logo_icon.svg" alt="ROMI logo" />
+        </NuxtLink>
       </div>
       <nav>
         <ul class="flex space-x-6">
