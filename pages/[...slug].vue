29f3022c0c9dda48f5133783bdd09b4975da7934
Nestor Vera <nestorvera@me.com>
2022-07-05 16:20:44 +0200

    Remove default page component

--- pages/[...slug].vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <article>
-    <ContentDoc
-      class="grid gap-8"
-      _class="prose max-w-none text-current prose-headings:text-current prose-a:text-current"
-    />
-  </article>
-</template>
