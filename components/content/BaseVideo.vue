35237fb523af25f808769593fa56e432e1692349
Nestor Vera <nestorvera@me.com>
2022-07-07 12:57:26 +0200

    Refactor video cards to their own component

---
+++ components/content/BaseVideo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="['grid gap-8 content-start', 'bg-white rounded-lg p-8', 'overflow-hidden']">
+    <figure v-if="item.id" class="-mt-8 -mx-8">
+      <EmbedYoutube :id="item.id" />
+    </figure>
+    <div v-if="item.description">
+      <p>{{ item.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  item: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
--- components/content/SectionVideos.vue
+++ components/content/SectionVideos.vue
@@ -3,13 +3,8 @@
     <slot />
 
     <ul v-if="items.length" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-      <li v-for="(item, key) in items" :key="key" class="grid gap-4 content-start" :class="['grid gap-8 content-start', 'bg-white rounded-lg p-8', 'overflow-hidden']">
-        <figure v-if="item.id" class="-mt-8 -mx-8">
-          <EmbedYoutube :id="item.id" />
-        </figure>
-        <div v-if="item.description">
-          <p>{{ item.description }}</p>
-        </div>
+      <li v-for="(item, key) in items" :key="key">
+        <BaseVideo v-bind="{ item }" />
       </li>
     </ul>
   </section>
