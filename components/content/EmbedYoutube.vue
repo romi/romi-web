b0551d2f83399b85528cb9cf774790065a886b37
Nestor Vera <nestorvera@me.com>
2022-07-11 00:09:12 +0200

    Add shadow to embed play button to increase contrast on light backgrounds

--- components/content/EmbedYoutube.vue
+++ components/content/EmbedYoutube.vue
@@ -6,7 +6,7 @@
     <template v-else>
       <img class="object-cover" :src="`http://i3.ytimg.com/vi/${id}/maxresdefault.jpg`" alt="">
       <button class="group flex justify-center items-center" @click="isActive = !isActive">
-        <div class="w-20 h-w-20 rounded-full bg-primary-400/75 group-hover:scale-105 group-focus:scale-105 transition-transform">
+        <div class="w-20 h-w-20 rounded-full bg-primary-400/75 group-hover:scale-105 group-focus:scale-105 transition-transform shadow">
           <svg xmlns="http://www.w3.org/2000/svg" class="w-full fill-primary-50" viewBox="0 0 20 20">
             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
           </svg>
