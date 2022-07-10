b84c9079a9efee20cfe486985227f76be9e84a84
Nestor Vera <nestorvera@me.com>
2022-07-10 13:37:18 +0200

    Require user interaction before rendering YouTube iframe

--- components/content/EmbedYoutube.vue
+++ components/content/EmbedYoutube.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="aspect-w-16 aspect-h-9">
-    <iframe :src="`https://www.youtube.com/embed/${id}`" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <template v-if="isActive">
+      <iframe :src="`https://www.youtube.com/embed/${id}?autoplay=1`" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </template>
+    <template v-else>
+      <img class="object-cover" :src="`http://i3.ytimg.com/vi/${id}/maxresdefault.jpg`" alt="">
+      <button class="group flex justify-center items-center" @click="isActive = !isActive">
+        <div class="w-20 h-w-20 rounded-full bg-primary-400/75 group-hover:scale-105 group-focus:scale-105 transition-transform">
+          <svg xmlns="http://www.w3.org/2000/svg" class="w-full fill-primary-50" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
+          </svg>
+        </div>
+      </button>
+    </template>
   </div>
 </template>
 
@@ -11,4 +23,6 @@ const props = defineProps({
     required: true,
   },
 })
+
+const isActive = ref(false)
 </script>
