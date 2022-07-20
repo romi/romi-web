929f1131d23e2dc1c09a372afff3a3aa88b99bdc
Nestor Vera <nestorvera@me.com>
2022-07-20 10:54:12 +0200

    Use `BaseCard` component for partner cards

--- components/content/SitePartners.vue
+++ components/content/SitePartners.vue
@@ -1,18 +1,22 @@
 <template>
   <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
     <ContentList path="/partners" v-slot="{ list }" >
-      <li v-for="article in list" :key="article._path" :class="['grid gap-4 content-start', 'bg-white rounded-lg p-8']">
-        <div class="flex">
-          <NuxtLink class="flex" :to="article.url" target="_blank">
-            <img class="w-auto h-16 object-contain object-left-bottom grayscale" :src="article.logo" :alt="`${article.title} logo`">
-          </NuxtLink>
-        </div>
-        <div class="grid gap-2">
-          <h2 class="flex !text-xl md:!text-2xl">
-            <NuxtLink class="flex" :to="article.url" target="_blank">{{ article.title }}</NuxtLink>
-          </h2>
-          <p>{{ article.description }}</p>
-        </div>
+      <li v-for="article in list" :key="article._path">
+        <BaseCard v-bind="article" class="h-full">
+          <template #media>
+            <NuxtLink class="flex pt-8 px-8" :to="article.url" target="_blank">
+              <img class="w-auto h-16 object-contain object-left-bottom grayscale" :src="article.logo" :alt="`${article.title} logo`">
+            </NuxtLink>
+          </template>
+          <template #title>
+            <h2 class="flex !text-xl md:!text-2xl">
+              <NuxtLink class="flex" :to="article.url" target="_blank">{{ article.title }}</NuxtLink>
+            </h2>
+          </template>
+          <template #default>
+            <p>{{ article.description }}</p>
+          </template>
+        </BaseCard>
       </li>
     </ContentList>
   </ul>
