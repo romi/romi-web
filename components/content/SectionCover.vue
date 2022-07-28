4e38304d25fb4f9ec2c10433cafc29627b84a0f0
Nestor Vera <nestorvera@me.com>
2022-07-28 17:42:10 +0200

    Fix text alignment in `SectionCover` component

--- components/content/SectionCover.vue
+++ components/content/SectionCover.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="w-full grid gap-16 text-center mb-8">
-    <div v-if="$slots.title || $slots.default" class="grid gap-8 w-full max-w-5xl mx-auto">
-      <div v-if="$slots.title" class="">
+  <div class="w-full grid gap-16 mb-8">
+    <div v-if="$slots.title || $slots.default" class="grid gap-8 w-full max-w-7xl">
+      <div v-if="$slots.title" class="max-w-5xl">
         <slot name="title" />
       </div>
-      <div v-if="$slots.default" class="text-primary-900/75 text-xl xl:text-3xl font-medium">
+      <div v-if="$slots.default" class="text-primary-900/75 text-xl xl:text-3xl font-medium max-w-4xl">
         <slot name="default" />
       </div>
     </div>
